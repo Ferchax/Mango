0e01eab617c539c19edeb1f67c2f7f7cc7dc2211
Ferchax <fgc.schlottmann@gmail.com>
2024-01-19 23:14:53 -0300

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,144 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        private ResponseDto response;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                /*CouponDto couponDto = new CouponDto()
+                {
+                    CouponId = obj.CouponId,
+                    CouponCode = obj.CouponCode,
+                    DisscountAmount = obj.DisscountAmount,
+                    MinAmount = obj.MinAmount
+                };*/
+                
+                response.Result = _mapper.Map<CouponDto>(obj); ;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponCode.ToLower() == code.ToLower());                 
+
+                response.Result = _mapper.Map<CouponDto>(obj); ;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                response.Result = _mapper.Map<CouponDto>(obj); ;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                response.Result = _mapper.Map<CouponDto>(obj); ;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
